c73f3b2048a6f432a5273ede2996d4d96f9c6d87
shobikasribk <144214525+shobikasribk@users.noreply.github.com>
2023-09-06 13:54:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def fact_rec(n):
+ if n==0:
+    return 1
+ else:
+    return n*fact_rec(n-1)
+number=int(input("enter a value:"))
+res=fact_rec(number)
+print("the factorial of {}.". format(number,res))
